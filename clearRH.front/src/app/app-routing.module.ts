a6add223c756c368ecbc7a91ea07f7478fe6581c
Mike D. Saint <douglasmikemd@gmail.com>
2023-01-24 16:20:04 -0300

    Estrutura de pastas

--- clearRH.front/src/app/app-routing.module.ts
+++ clearRH.front/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPageComponent } from './admin/admin-page/admin-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'adminPage', component: AdminPageComponent },
+  { path: 'userPage', component: UserPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
