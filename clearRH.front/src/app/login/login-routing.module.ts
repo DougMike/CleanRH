14c56731b493207b7b37ee312c66e5dc52912a2f
Mike D. Saint <douglasmikemd@gmail.com>
2023-02-04 10:17:26 -0300

    .

--- clearRH.front/src/app/login/login-routing.module.ts
+++ clearRH.front/src/app/login/login-routing.module.ts
@@ -1,21 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '../pages/home/home.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
-  {
-    path: 'admin',
-    loadChildren: () => import('../admin/admin.module').then(m => m.AdminModule)
-  },
-  {
-    path: 'user',
-    loadChildren: () => import('../user/user.module').then(m => m.UserModule)
-  }
+  { path: '', component: LoginComponent }
 ];
 
 @NgModule({
